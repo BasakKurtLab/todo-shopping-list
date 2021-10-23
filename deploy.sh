634963eef45275509ad321a1eea82d07c016ee16
BasakKurtLab <79793959+BasakKurtLab@users.noreply.github.com>
2021-10-23 23:53:22 +0200

    commit

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:basakkurtlab/todo-shopping-list.git main:gh-pages
+git push -f git@github.com:BasakKurtLab/todo-shopping-list.git main:gh-pages
 
 cd -
